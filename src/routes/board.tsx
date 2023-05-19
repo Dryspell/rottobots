f85d948a8345e3abdc37013a44ca7d72c12fff05
Noah <NhWinslow@gmail.com>
2023-05-18 22:32:29 -0500

    Cleanup

--- src/routes/board.tsx
+++ src/routes/board.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { Accessor, Setter, createSignal } from "solid-js";
 import { Button } from "@suid/material";
 import { behaviorTable } from "../../lib/transitionTables";
 
@@ -32,6 +32,72 @@ const generateNeighborStateString = (neighbors: number[]) => {
 	return neighborStateString;
 };
 
+const ClearButton = (props: { setState: Setter<number[][]> }) => {
+	return (
+		<button
+			aria-label="clear"
+			class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
+			onClick={() =>
+				props.setState(
+					Array.from(Array(ROW_COUNT).keys()).map((k) =>
+						Array.from(Array(COLUMN_COUNT).keys()).map((k) => 0)
+					)
+				)
+			}
+		>
+			Clear
+		</button>
+	);
+};
+
+const RandomizeButton = (props: { setState: Setter<number[][]> }) => (
+	<button
+		aria-label="randomize"
+		class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
+		onClick={() =>
+			props.setState(
+				Array.from(Array(ROW_COUNT).keys()).map((k) =>
+					Array.from(Array(COLUMN_COUNT).keys()).map((k) =>
+						Math.round(Math.random())
+					)
+				)
+			)
+		}
+	>
+		Randomize
+	</button>
+);
+
+const UpdateButton = (props: {
+	state: Accessor<number[][]>;
+	setState: Setter<number[][]>;
+}) => (
+	<button
+		aria-label="update state"
+		class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
+		onClick={() => {
+			const newState = [...props.state()];
+			for (let i = 0; i < ROW_COUNT; i++) {
+				for (let j = 0; j < COLUMN_COUNT; j++) {
+					const neighborStateString = generateNeighborStateString(
+						getNeighbors(i, j, props.state)
+					);
+					// console.log(neighborStateString);
+
+					newState[i][j] =
+						behaviorTable["gol"][props.state()[i][j]].transitions[
+							neighborStateString
+						] || behaviorTable["gol"][props.state()[i][j]].default;
+				}
+			}
+			console.log(newState);
+			props.setState(newState);
+		}}
+	>
+		Update State
+	</button>
+);
+
 export default function Board() {
 	const [state, setState] = createSignal<number[][]>(
 		Array.from(Array(ROW_COUNT).keys()).map((k) =>
@@ -44,62 +110,11 @@ export default function Board() {
 	return (
 		<div class="">
 			<div class="p-5 flex justify-center">
-				<button
-					aria-label="clear"
-					class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-					onClick={() =>
-						setState(
-							Array.from(Array(ROW_COUNT).keys()).map((k) =>
-								Array.from(Array(COLUMN_COUNT).keys()).map(
-									(k) => 0
-								)
-							)
-						)
-					}
-				>
-					Clear
-				</button>
-				<button
-					aria-label="randomize"
-					class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-					onClick={() =>
-						setState(
-							Array.from(Array(ROW_COUNT).keys()).map((k) =>
-								Array.from(Array(COLUMN_COUNT).keys()).map(
-									(k) => Math.round(Math.random())
-								)
-							)
-						)
-					}
-				>
-					Randomize
-				</button>
-				<button
-					aria-label="update state"
-					class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-					onClick={() => {
-						const newState = [...state()];
-						for (let i = 0; i < ROW_COUNT; i++) {
-							for (let j = 0; j < COLUMN_COUNT; j++) {
-								const neighborStateString =
-									generateNeighborStateString(
-										getNeighbors(i, j, state)
-									);
-								// console.log(neighborStateString);
-
-								newState[i][j] =
-									behaviorTable["gol"][state()[i][j]]
-										.transitions[neighborStateString] ||
-									behaviorTable["gol"][state()[i][j]].default;
-							}
-						}
-						console.log(newState);
-						setState(newState);
-					}}
-				>
-					Update State
-				</button>
+				<ClearButton setState={setState} />
+				<RandomizeButton setState={setState} />
+				<UpdateButton state={state} setState={setState} />
 			</div>
+
 			<div class="p-5 flex justify-center">
 				<div aria-label="board">
 					{state().map((row, rowIndex) => (
@@ -109,12 +124,13 @@ export default function Board() {
 									class={`${behaviorTable["gol"][value].color} p-0 m-0 rounded-md w-10 h-10 inline-block float-left clear-none`}
 									aria-label="cell"
 									onClick={() => {
+										// console.log(
+										// 	behaviorTable["gol"][value].color
+										// );
 										const newState = [...state()];
-										newState[rowIndex][colIndex] = state()[
-											rowIndex
-										][colIndex]
-											? 0
-											: 1;
+										newState[rowIndex][colIndex] =
+											(state()[rowIndex][colIndex] + 1) %
+											behaviorTable["gol"].length;
 										setState(newState);
 									}}
 								>
