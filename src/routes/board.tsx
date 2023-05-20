2a036f1147c114973169e4472a2300cbaaa649a9
Noah <NhWinslow@gmail.com>
2023-05-20 14:18:06 -0500

    Working GOL spinner!

--- src/routes/board.tsx
+++ src/routes/board.tsx
@@ -2,22 +2,38 @@ import { Accessor, Setter, createSignal } from "solid-js";
 import { Button } from "@suid/material";
 import { behaviorTable } from "../../lib/transitionTables";
 
-const COLUMN_COUNT = 20;
-const ROW_COUNT = 14;
+const COLUMN_COUNT = 3;
+const ROW_COUNT = 3;
+const TOROIDAL = false;
 
 const getNeighbors = (i: number, j: number, state: () => number[][]) => {
-	const neighbors = [
-		state()[(i - 1 + ROW_COUNT) % ROW_COUNT][
-			(j - 1 + COLUMN_COUNT) % COLUMN_COUNT
-		],
-		state()[(i - 1 + ROW_COUNT) % ROW_COUNT][j],
-		state()[(i - 1 + ROW_COUNT) % ROW_COUNT][(j + 1) % COLUMN_COUNT],
-		state()[i][(j - 1 + COLUMN_COUNT) % COLUMN_COUNT],
-		state()[i][(j + 1) % COLUMN_COUNT],
-		state()[(i + 1) % ROW_COUNT][(j - 1 + COLUMN_COUNT) % COLUMN_COUNT],
-		state()[(i + 1) % ROW_COUNT][j],
-		state()[(i + 1) % ROW_COUNT][(j + 1) % COLUMN_COUNT],
-	];
+	const neighbors = TOROIDAL
+		? [
+				state()[(i - 1 + ROW_COUNT) % ROW_COUNT][
+					(j - 1 + COLUMN_COUNT) % COLUMN_COUNT
+				],
+				state()[(i - 1 + ROW_COUNT) % ROW_COUNT][j],
+				state()[(i - 1 + ROW_COUNT) % ROW_COUNT][
+					(j + 1) % COLUMN_COUNT
+				],
+				state()[i][(j - 1 + COLUMN_COUNT) % COLUMN_COUNT],
+				state()[i][(j + 1) % COLUMN_COUNT],
+				state()[(i + 1) % ROW_COUNT][
+					(j - 1 + COLUMN_COUNT) % COLUMN_COUNT
+				],
+				state()[(i + 1) % ROW_COUNT][j],
+				state()[(i + 1) % ROW_COUNT][(j + 1) % COLUMN_COUNT],
+		  ]
+		: [
+				state()[i - 1]?.[j - 1],
+				state()[i - 1]?.[j],
+				state()[i - 1]?.[j + 1],
+				state()[i]?.[j - 1],
+				state()[i]?.[j + 1],
+				state()[i + 1]?.[j - 1],
+				state()[i + 1]?.[j],
+				state()[i + 1]?.[j + 1],
+		  ].map((neighbor) => neighbor ?? 0);
 	return neighbors;
 };
 
@@ -33,8 +49,8 @@ const generateNeighborStateString = (neighbors: number[]) => {
 };
 
 const emptyMatrix = (rows: number, cols: number) =>
-	Array.from(Array(ROW_COUNT).keys()).map(() =>
-		Array.from(Array(COLUMN_COUNT).keys()).map(() => 0)
+	Array.from(Array(rows).keys()).map(() =>
+		Array.from(Array(cols).keys()).map(() => 0)
 	);
 
 const ClearButton = (props: { setState: Setter<number[][]> }) => {
@@ -75,20 +91,32 @@ const UpdateButton = (props: {
 		aria-label="update state"
 		class="rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
 		onClick={() => {
-			const newState = [...props.state()];
-			for (let i = 0; i < ROW_COUNT; i++) {
-				for (let j = 0; j < COLUMN_COUNT; j++) {
-					const neighborStateString = generateNeighborStateString(
-						getNeighbors(i, j, props.state)
-					);
-					// console.log(neighborStateString);
+			console.log(`----- UPDATE STATE -----`);
 
-					newState[i][j] =
-						behaviorTable["gol"][props.state()[i][j]].transitions[
+			const newState = emptyMatrix(ROW_COUNT, COLUMN_COUNT);
+			for (let ri = 0; ri < ROW_COUNT; ri++) {
+				for (let ci = 0; ci < COLUMN_COUNT; ci++) {
+					const neighborStateString = generateNeighborStateString(
+						getNeighbors(ri, ci, props.state)
+					);
+
+					const newLocalState =
+						behaviorTable["gol"][props.state()[ri][ci]].transitions[
 							neighborStateString
-						] || behaviorTable["gol"][props.state()[i][j]].default;
+						] ??
+						behaviorTable["gol"][props.state()[ri][ci]].default;
+
+					newState[ri][ci] = newLocalState;
+
+					// state()[ri][ci] &&
+					console.log(
+						`(${ri}, ${ci}): ${generateNeighborStateString(
+							getNeighbors(ri, ci, props.state)
+						)}, newLocalState: ${newLocalState}`
+					);
 				}
 			}
+
 			console.log(newState);
 			props.setState(newState);
 		}}
@@ -116,21 +144,25 @@ export default function Board() {
 
 			<div class="p-5 flex justify-center">
 				<div aria-label="board">
-					{state().map((row, rowIndex) => (
+					{state().map((row, ri) => (
 						<div class="block overflow-hidden p-0">
-							{row.map((value, colIndex) => (
+							{row.map((value, ci) => (
 								<div
 									class={`${behaviorTable["gol"][value].color} p-0 m-0 rounded-md w-10 h-10 inline-block float-left clear-none`}
 									aria-label="cell"
 									onClick={() => {
-										// console.log(
-										// 	behaviorTable["gol"][value].color
-										// );
 										const newState = [...state()];
-										newState[rowIndex][colIndex] =
-											(state()[rowIndex][colIndex] + 1) %
+										newState[ri][ci] =
+											(state()[ri][ci] + 1) %
 											behaviorTable["gol"].length;
 										setState(newState);
+
+										// state()[ri][ci] &&
+										console.log(
+											`(${ri}, ${ci}): ${generateNeighborStateString(
+												getNeighbors(ri, ci, state)
+											)}`
+										);
 									}}
 								>
 									{/* <span>{`(${rowIndex}, ${colIndex})`}</span> */}
